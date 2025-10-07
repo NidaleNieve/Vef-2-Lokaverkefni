0d28bbf1f02b4166e1e75eae64f0df6ed01c4b40
NidaleNieve <nidalenieve@gmail.com>
2025-10-07 10:48:46 +0000

    surpress hydration warning

--- flamed/app/layout.js
+++ flamed/app/layout.js
@@ -19,7 +19,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" suppressHydrationWarning className={`${geistSans.variable} ${geistMono.variable}`}>
       <body
         suppressHydrationWarning
         className="antialiased"
